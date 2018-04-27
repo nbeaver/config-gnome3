9e924c74cc436750a6adf90b12fdc2dcd2344fef
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-04-27 11:13:57 -0500

    Disable cursor blinking in gnome-terminal.

--- my-gnome-config.sh
+++ my-gnome-config.sh
@@ -1,3 +1,6 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 gsettings set org.gnome.desktop.interface clock-format 12h
+
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')/ cursor-blink-mode off
+# https://geoff.greer.fm/2016/08/26/gnome-terminal-cursor-blinking-saga/
