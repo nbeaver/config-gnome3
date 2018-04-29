e49737e34b5c60064b9b8ca92c5dd5b9029320a9
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-04-28 22:56:20 -0500

    Turn off alert noises.

--- my-gnome-config.sh
+++ my-gnome-config.sh
@@ -6,3 +6,7 @@ gsettings set org.gnome.desktop.interface clock-format 12h
 # Turn off cursor blinking.
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')/ cursor-blink-mode off
 # https://geoff.greer.fm/2016/08/26/gnome-terminal-cursor-blinking-saga/
+
+gsettings set org.gnome.desktop.sound event-sounds false
+# https://help.ubuntu.com/stable/ubuntu-help/sound-alert.html
+# https://askubuntu.com/questions/25531/how-to-disable-alert-volume-from-the-command-line#178480
